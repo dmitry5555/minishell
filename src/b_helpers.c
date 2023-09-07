408cfa6ccbb6c23afb0c75e5ad77793c38bf0003
Dmitrii Larionov <dlariono@z1t1c1.42bangkok.com>
2023-09-07 18:43:38 +0700

    ft_is_builtin added

--- src/b_helpers.c
+++ src/b_helpers.c
@@ -1,7 +1,30 @@
 # include "minishell.h"
 
 
-//ft_lstadd_back
+void ft_is_builtin()
+{
+	int	size;
+
+	char *test = "echo";
+	const char* b_in[] =
+	{
+		"echo",
+		"cd",
+		"pwd",
+		"export",
+		"unset",
+		"env",
+		"exit"
+	};
+
+	size = sizeof(b_in) / sizeof(b_in[0]);
+	while(size--)
+	{
+		if (!ft_strcmp(b_in[size], test))
+			printf("%s is builtin \n",test);
+
+	}
+}
 
 // void ft_find_right_path(t_list **t_cmd_node)
 void ft_find_right_path()
@@ -13,13 +36,13 @@ void ft_find_right_path()
 
 	const char* arr[] =
 	{
-        "/usr/local/bin",
-        "/usr/bin",
-        "/bin",
-        "/usr/sbin",
-        "/sbin",
-        "/usr/local/munki"
-    };
+		"/usr/local/bin",
+		"/usr/bin",
+		"/bin",
+		"/usr/sbin",
+		"/sbin",
+		"/usr/local/munki"
+	};
 
 	total = sizeof(arr) / sizeof(arr[0]);
 
@@ -34,18 +57,19 @@ void ft_find_right_path()
 			printf("%s is accessible \n", ft_strjoin(ft_strjoin(arr[total],"/"), cmd));
 		else
 			printf("%s is not accessible \n", ft_strjoin(ft_strjoin(arr[total],"/"), cmd));
+		// total--;
 
 	}
 	// if (access("/usr/local/munki/ls", X_OK) == 0)
 	// 	printf("good path");
 	// const char* executable = "/usr/local/munki";
 
-    // Check if the executable can be accessed
-    // if (access(executable, X_OK) == 0) {
-    //     printf("%s is accessible\n", executable);
-    // } else {
-    //     printf("%s is not accessible\n", executable);
-    // }
+	// Check if the executable can be accessed
+	// if (access(executable, X_OK) == 0) {
+	//     printf("%s is accessible\n", executable);
+	// } else {
+	//     printf("%s is not accessible\n", executable);
+	// }
 }
 
 // int main()
@@ -64,4 +88,5 @@ void ft_find_right_path()
 // 	// write(1,&paths[1][1],1);
 
 // 	ft_find_right_path();
+// 	ft_is_builtin();
 // }
