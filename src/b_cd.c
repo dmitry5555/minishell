da8a0faeb92ed2f9a3f6b89d04d70fe7748afa70
Dmitrii Larionov <dlariono@z1t1c1.42bangkok.com>
2023-09-08 15:55:58 +0700

    header

--- src/b_cd.c
+++ src/b_cd.c
@@ -10,18 +10,19 @@ void	ft_cd(t_list **env, char *name, char *content, int is_env)
 
 // int	main()
 // {
-// 	// while ((input = readline("Enter something: ")) != NULL)
-// 	// {
-// 	// 	// modify_and_redisplay(input);
-// 	// 	if (input)
-// 	// 	{
-// 	// 		// rl_on_new_line();
-// 	// 		// rl_replace_line("111", "123");
-// 	// 		// add_history("132");
-// 	// 		// rl_replace_line("123");
-// 	// 		// clear_history();
-// 	// 	}
-// 	// 	free(input);
-// 	// }
-// 	// return (0);
+// 	char *input;
+// 	while ((input = readline("Enter something: ")) != NULL)
+// 	{
+// 		// modify_and_redisplay(input);
+// 		if (input)
+// 		{
+// 			// rl_on_new_line();
+// 			// rl_replace_line("111", "123");
+// 			// add_history("132");
+// 			// rl_replace_line("123");
+// 			// clear_history();
+// 		}
+// 		free(input);
+// 	}
+// 	return (0);
 // }
