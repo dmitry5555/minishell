588ac73fb26b0fde232f3e622969d74f9e552257
justin daly <justin.m.daly@gmail.com>
2023-09-11 22:20:32 +0700

    add strlcat

--- includes/minishell.h
+++ includes/minishell.h
@@ -47,6 +47,8 @@ void	ft_putendl_fd(char *str, int fd);
 char	*ft_strchr(const char *str, int c);
 size_t	ft_strlen(const char *a);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strndup(const char *s, size_t n);
 char	*ft_strdup(const char *s1);
