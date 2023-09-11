edeb78e8fc733096f1467b3363eea0ab5916a86f
justin daly <justin.m.daly@gmail.com>
2023-09-11 22:20:51 +0700

    add fns to expander

--- src/expander.c
+++ src/expander.c
@@ -6,11 +6,51 @@
 /*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 18:40:51 by jdaly             #+#    #+#             */
-/*   Updated: 2023/09/10 22:52:40 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/09/11 22:18:35 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
+
+/* cases to handle
+    1. $VARIABLE
+    2. $VARIABLE_$VARIABLE --> jdaly
+    3. $? --> expand to exit status
+    4. $$ --> expand to pid
+
+    5. ~ --> /Users/jdaly
+    6. ~jdaly --> /Users/jdaly
+    
+*/
+
+int	ft_isalnum(int c)
+{
+	if (!(((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+			|| (c >= '0' && c <= '9')))
+		return (0);
+	else
+		return (1);
+}
+
+int	ft_isequalstrn(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (s1 == s2)
+		return (1);
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	i = 0;
+	while (!(s1[i] == '\0' && s2[i] == '\0'))
+	{
+		if (n > 0 && i >= n)
+			break ;
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 // void expand_vars(char *str, t_list *envlist)
 // {
@@ -63,63 +103,179 @@
 //     }
 // }
 
-void expand_vars(char *str, t_list *envlist)
+// char    *get_varname(char *str)
+// {
+//     char *varname;
+//     int i = -1;
+
+//     varname = NULL;
+//     while (str[++i])
+//     {
+//         if (str[i] == '$' && str[i + 1] != ' ' && str[i + 1])
+//         {
+//             i++;
+//             int j = 0;
+//             if (str[i] == '$')
+//             {
+//                 varname = strdup("$$");
+//                 break ;
+//             }
+//             while (str[i] != ' ' && str[i] && ft_isalnum(str[i]) || str[i] == '_')
+//             {
+//                 j++;
+//                 i++;
+//             }
+
+//             if (j > 0)
+//             {
+//                 varname = strndup(&str[i - j], j);
+//                 varname[j] = '\0';
+//                 break ;
+//             }
+//         }
+//     }
+//     return (varname);
+// }
+
+// void expand_vars_recursive(char *str, t_list *envlist)
+// {
+//     char    *result;
+//     char    *varname;
+//     char    *varvalue;
+//     int     varfound;
+    
+//     result = (char *)malloc(strlen(str) + 1); // Initialize an empty string
+//     strcpy(result, ""); // Use strcpy for the initial empty string
+//     int i = 0;
+//     varfound = 0;
+//     while (str[i])
+//     {
+//         varfound = 1;
+//         if (str[i] == '$' && str[i + 1] != ' ' && str[i + 1])
+//         {
+//             i++;
+//             int j = 0;
+//             while (str[i] != ' ' && str[i])
+//             {
+//                 j++;
+//                 i++;
+//             }
+//             if (j > 0) 
+//             {
+//                 varname = (char *)malloc(j + 1); // Allocate memory for variable name
+//                 strncpy(varname, &str[i - j], j);
+//                 varname[j] = '\0';
+
+//                 varvalue = get_content_by_name(envlist, varname);
+//                 free(varname);
+
+//                 if (varvalue) {
+//                     strcat(result, varvalue);
+//                 }
+//                 // No variable found, skip the $variable (including '$')
+//             }
+//         }
+//         else {
+//             // Copy characters other than '$' and spaces to the result
+//             char temp[2] = {str[i], '\0'};
+//             strcat(result, temp);
+//             i++;
+//         }
+//     }
+
+//     // Update the original string
+//     if (varfound)
+//         strcpy(str, result);
+//     free(result);
+
+//     // Check if there are more variables to expand recursively
+//     if (varfound && strstr(str, "$"))
+//         expand_vars_recursive(str, envlist);
+// }
+
+char    *get_varname(char *str, int bgn, int *end)
 {
-    char *result = (char *)malloc(strlen(str) + 1); // Initialize an empty string
-    ft_strlcpy(result, "", 1);
+    int tmp_end;
+    
+    tmp_end = bgn;
+    while (str[tmp_end] && str[tmp_end] != ' ' && str[bgn] && ft_isalnum(str[bgn]) || str[bgn] == '_')
+        tmp_end++;
+    *end = tmp_end;
+    return (ft_strndup(&str[bgn], tmp_end - bgn));
+}
 
-    int i = 0;
+// void    expand_all(char **args, t_list envlist)
+// {
+//     while (args)
+//     {
+//         if (args[i][0] != '\'' && args[i][ft_strlen(args[i] - 1) != '\''])
+//             expand_vars(args[i]);
+        
+//     }
+// }
 
-    while (str[i])
+char    *expand_vars(char *str, t_list *envlist)
+{
+    int     i;
+    char    *varname;
+    char    *varvalue;
+    char    *tmp_result;
+    char    *tmp_char;
+    char    *result;
+    int     var_end;
+
+    result = ft_strdup("");
+    i = -1;
+    var_end = 0;
+    while (str[++i])
     {
-        if (str[i] == '$' && str[i + 1] != ' ' && str[i + 1])
+        if (str[i] == '$' && (ft_isalnum(str[i + 1]) || str[i + 1] == '_'))
         {
             i++;
-            int j = 0;
-            while (str[i] != ' ' && str[i])
+            varname = get_varname(str, i, &var_end);
+            printf("varname = %s\n", varname);
+            varvalue = get_content_by_name(envlist, varname);
+            printf("varvalue = %s\n", varvalue);
+            free(varname);
+            if (varvalue)
             {
-                j++;
+                tmp_result = ft_strjoin(result, varvalue);
+                if (tmp_result)
+                {
+                    free(result);
+                    result = tmp_result;
+                    printf("tmp result: %s\n", tmp_result);
+                }
+                i = var_end;
+            }
+            else
                 i++;
-            }
-
-            if (j > 0)
-            {
-                char *varname = (char *)malloc(j + 1); // Allocate memory for variable name
-                strncpy(varname, &str[i - j], j);
-                varname[j] = '\0';
-
-                char *varvalue = get_content_by_name(envlist, varname);
-                free(varname);
-
-                if (varvalue)
-                    strcat(result, varvalue);
-                    // No variable found, skip the $variable (including '$')
-            }
         }
         else
         {
-            // Copy characters other than '$' and spaces to the result
-            char temp[2] = {str[i], '\0'};
-            ft_strlcat(result, temp, ft_strlen(result) + ft_strlen(temp));
-            i++;
+            tmp_char = strndup(&str[i], 1);
+            //printf("tmp_char = %s\n", tmp_char);
+            result = ft_strjoin(result, tmp_char);
+            free(tmp_char);
         }
     }
-
-    // Update the original string
-    ft_strlcpy(str, result, ft_strlen(str) + ft_strlen(result));
-    free(result);
+    return (result);
 }
 
 // int main(void)
 // {
-//     char *str = ft_strdup("hello $novar your home directory is: $HOME");
+//     char *str = ft_strdup("hello $USER");
 //     t_list *envlist = ft_env();
+//     char *varname;
 
+//     // varname = get_varname(str);
+//     // printf("varname = %s\n", varname);
 //     expand_vars(str, envlist);
 //     printf("str = %s\n", str);
 
 //     // Don't forget to free memory allocated for str and envlist when done.
 //     free(str);
+//     //free(varname);
 //     //ft_lstclear(envlist, &del);
 //     return 0;
 // }
