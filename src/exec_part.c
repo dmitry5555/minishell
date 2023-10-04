2888ed73627e7134d3cb10f0a4628ffb299d6216
Dmitrii Larionov <mikrolux@gmail.com>
2023-10-04 15:57:24 +0700

    testing new pipes

--- src/exec_part.c
+++ src/exec_part.c
@@ -28,56 +28,169 @@ void	print_cmd_list(t_cmdlist *cmd_list)
 		}
 }
 
+void single_node(t_cmd_node *node)
+{
+	int fd[2];
+	pid_t pid;
+
+	pipe(fd);
+	pid = fork();
+
+	if (pid!=0) // parent - accepting process
+	{
+		dup2(fd[1], 1);
+		close(fd[0]);
+		execvp(node->cmd[0], node->cmd);
+		perror("execvp");
+	}
+	else
+	{
+		dup2(fd[0],0);
+		close(fd[1]);
+	}
+
+
+}
+
+void	run_cmds(t_cmdlist *cmd_list)
+{
+	t_cmd_node *node;
+
+	// while(cmd_list)
+	// {
+	node = (t_cmd_node *)cmd_list->content;
+	single_node(node);
+
+	// 	cmd_list = cmd_list->next;
+	// }
+	perror("error");
+}
+
+
+void run_single(t_cmdlist *cmd_list)
+{
+	t_cmd_node *node1;
+	t_cmd_node *node2;
+	t_cmd_node *node3;
+
+	node1 = (t_cmd_node *)cmd_list->content;
+	node2 = (t_cmd_node *)cmd_list->next->content;
+	// node3 = (t_cmd_node *)cmd_list->next->next->content;
+
+	// char *arr1[] = {"ls", "ls", "-1"};
+	// char *arr2[] = {"wc", "wc", "-l"};
+	// char *arr3[] = {"grep", "grep", "5"};
+	int fd[2];
+	pid_t pid;
+
+	pipe(fd);
+	pid = fork();
+
+	if (pid != 0)
+	{
+		dup2(fd[1],1);
+		close(fd[0]);
+		execvp(node1->cmd[0], node1->cmd);
+		// execlp("ls", "ls", "-1", NULL);
+		perror("execvp");
+	}
+	else
+	{
+		dup2(fd[0], 0);
+		close(fd[1]);
+		execvp(node2->cmd[0], node2->cmd);
+		perror("execvp");
+	}
+
+}
 
 void	exec_all(char *out, t_list *env)
 {
 	char **args;
-	int		shlvl;
+	// int		shlvl;
 	t_cmdlist *cmd_list;
 	t_cmd_node *node;
 
 	cmd_list = NULL;
-	if (!out)
-	{
-		printf("exit\n");
-		return ;
-	}
+	// if (!out)
+	// {
+	// 	printf("exit\n");
+	// 	return ;
+	// }
 	if (out[0] != '\0')
 		add_history(out);
-	args = ft_split_cmds(out, " ");
+
+	args = ft_split_cmds(out, " "); // we get an array with cmds
 	if (!args)
 		ft_error(ERR_QUOTE, NULL, 2);
 	if (args)
 	{
-		cmd_list = create_cmd_list(final_split(args, env), -1);
-		print_cmd_list(cmd_list);
-		ft_cmdlstclear(&cmd_list, free_cmd_content);
+		cmd_list = create_cmd_list(final_split(args, env), -1); // expand arr -> LL
+		// ft_find_right_paths(cmd_list);
+		// print_cmd_list(cmd_list);
+		// run_cmds(cmd_list);
+		// ft_cmdlstclear(&cmd_list, free_cmd_content);
 		// printf("AFTER FT_SPLIT_CMDS:\n");
 		// ft_print_array(args);
+
 	}
+	run_single(cmd_list);
 	free(out);
 }
 
 
+// int	main(int argc, char *argv[], char **env)
+// {
+// 	t_cmd_node	*node;
+// 	t_cmdlist	*current;
+// 	t_list		*env_list;
+
+// 	char		*out = NULL;
+// 	// char		*shlvl;
+
+// 	env_list = ft_env(env);
+// 	// out = readline("guest@minishell $ ");
+
+// 	while (argc && argv)
+// 	// {
+// 	// 	// signal(SIGINT, SIG_DFL);
+// 	// 	// signal(SIGQUIT, SIG_DFL);
+
+// 		out = readline("guest@minishell $ ");
+// 		exec_all(out, env_list);
+// 	}
+
 int	main(int argc, char *argv[], char **env)
 {
-	t_cmd_node	*node;
-	t_cmdlist	*current;
 	t_list		*env_list;
-
+	// t_cmd_node	*node;
+	// t_cmdlist	*current;
+	// t_cmdlist	*cmd_list;
+	// char		*start;
 	char		*out;
-	char		*shlvl;
-	out = NULL;
+	// char		*shlvl;
+	// cmd_list = NULL;
 
+	// env = NULL;
+	// char *out = "ls -1 | wc -l | grep src";
 	env_list = ft_env(env);
 
-	while (argc && argv)
+	// char *arr1[] = {"ls", "ls", "-1"};
+	// char *arr2[] = {"wc", "wc", "-l"};
+	// char *arr3[] = {"grep", "grep", "5"};
+
+	while (1)
 	{
 		signal(SIGINT, SIG_DFL);
 		signal(SIGQUIT, SIG_DFL);
 
-		out = readline("guest@minishell $ ");
+	// 	if (start)
+	// 		out = readline(start);
+	// 	else
+			out = readline("guest@minishell $ ");
+	// 	free(start);
 		exec_all(out, env_list);
-
+	// 	exec_all(out, envZ);
 	}
+	// ft_cmdlstclear(&cmd_list, free_cmd_content);
 }
--- src/test.c
+++ src/test.c
@@ -1,72 +1,30 @@
 # include "minishell.h"
 
-
 // int main()
 // {
-// 	t_list		*env;
-// 	t_cmdlist 	*cmd_list;
-// 	char		*env_paths;
-// 	char		**arr_paths;
 
-// 	// read currenv ENV, find 'PATH' content
-// 	env = ft_env();
-// 	env_paths = get_content_by_name(env, "PATH");
+// 	char *arr1[] = {"ls", "ls", "-1"};
+// 	char *arr2[] = {"wc", "wc", "-l"};
+// 	char *arr3[] = {"grep", "grep", "5"};
+// 	int fd[2];
+// 	pid_t pid;
 
-// 	// we have ARR with every path
-// 	arr_paths = split_string(env_paths, ":");
+// 	pipe(fd);
+// 	pid = fork();
 
-// 	// array of commands for cmd_list, will be in s_cmdlist
-// 	char **arr_cmd = malloc(sizeof(char *) * 7);
-// 	arr_cmd[0] = strdup("ls");
-// 	arr_cmd[1] = strdup("-la");
-// 	arr_cmd[2] = strdup("|");
-// 	arr_cmd[3] = strdup("pwd");
-// 	arr_cmd[4] = strdup("grep");
-// 	arr_cmd[5] = strdup("cd");
-// 	arr_cmd[6] = NULL;
-// 	cmd_list = create_cmd_list(arr_cmd, -1);
-
-// 	if (cmd_list)
+// 	if (pid != 0)
 // 	{
-// 		// Traverse the command list and print the commands
-// 		t_cmdlist *current;
-// 		int i;
-// 		current = cmd_list;
-
-// 		while (current)
-// 		{
-// 			t_cmd_node *node = (t_cmd_node *)current->content;
-// 			char *tmp1;
-
-// 			i = 0;
-// 			while (node->cmd[i])
-// 			{
-// 				// tmp1 = ft_strdup( ft_find_right_path(node->cmd[i]));
-
-// 				if (ft_is_builtin(node->cmd[i]))
-// 				{
-// 					printf("🟢 MAIN @ test.c \n	[%s]is builtin, can run \n\n", node->cmd[i]);
-// 					exec_cmd(node->cmd, 0, 1);
-// 				}
-// 				else if (ft_find_right_path(node->cmd[i], node->path))
-// 				{
-// 					printf("❌ MAIN @ test.c \n	exit in return 1 \n\n");
-// 					// return(0);
-// 				}
-// 				exec_cmd(node->cmd, 0, 1);
-
-// 				i++;
-// 			}
-// 			current = current->next;
-// 		}
-
-// 		// Free the memory allocated for the command list
+// 		dup2(fd[1],1);
+// 		close(fd[0]);
+// 		execlp("ls", "ls", "-1", NULL);
+// 		perror("execlp");
 // 	}
-
-// 	ft_free_list(env);
-// 	ft_free_arr(arr_paths);
-// 	// ft_array_free(arr_cmd);
-// 	ft_cmdlstclear(&cmd_list, free_cmd_content);
-
+// 	else
+// 	{
+// 		dup2(fd[0], 0);
+// 		close(fd[1]);
+// 		execlp("wc", "wc", "-l", NULL);
+// 		perror("execlp");
+// 	}
 
 // }
