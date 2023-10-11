f00da60de2228357d54f23284828ed66d33e43ab
justin daly <justin.m.daly@gmail.com>
2023-10-11 23:18:14 +0700

    fix norm

--- src/parser.c
+++ src/parser.c
@@ -3,34 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlariono <dlariono@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 14:10:00 by jdaly             #+#    #+#             */
-/*   Updated: 2023/10/11 19:06:48 by dlariono         ###   ########.fr       */
+/*   Updated: 2023/10/11 23:17:39 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "minishell.h"
+#include "minishell.h"
 
-char **final_split(char **args, t_list *envlist)
+char	**final_split(char **args, t_list *envlist)
 {
-	// expand_all(args, envlist);
-	// args = ft_subsplit(args, "<|>", -1);
-	// return (args);
 	char	**subsplit;
 	int		i;
 
 	i = -1;
 	subsplit = NULL;
-
 	while (args && args[++i])
 	{
 		args[i] = expand_vars(args[i], -1, envlist);
 		args[i] = expand_home(args[i], envlist);
 		subsplit = ft_subsplit(args[i], "<|>");
-		//ft_print_array(subsplit);
 		ft_array_replace_in(&args, subsplit, i);
-		i += ft_array_len(subsplit) - 1;
+		i += ft_a_len(subsplit) - 1;
 		ft_array_free(&subsplit);
 	}
 	return (args);
