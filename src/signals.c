68f30cf5124162c2e70d331168e020505b5fb780
justin daly <justin.m.daly@gmail.com>
2023-10-12 21:59:17 +0700

    fix norm

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 17:55:59 by dlariono          #+#    #+#             */
-/*   Updated: 2023/10/12 21:22:16 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/10/12 21:57:05 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@ void	sig_hand(int sig)
 {
 	if (sig == SIGINT)
 	{
-		//printf("ign sign = ctrl_c\n");
 		g_status = 130;
-		ioctl(STDIN_FILENO, TIOCSTI , "\n"); //mimics pressing the Enter key
+		ioctl(STDIN_FILENO, TIOCSTI, "\n"); //mimics pressing the Enter key
 		rl_replace_line("", 0); //clears out;
 		rl_on_new_line(); //moves cursor to new line
 	}
